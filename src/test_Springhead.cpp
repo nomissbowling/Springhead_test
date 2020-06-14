9a3cb3ca145e80f804068965bea77aa3ee4dc3c8
nomissbowling <nomissbowling@yahoo.co.jp>
2020-06-14 13:45:07 +0900

    practice

--- src/test_Springhead.cpp
+++ src/test_Springhead.cpp
@@ -18,18 +18,34 @@ public:
     FWApp::Init(ac, av);
     PHSdkIf *phSdk = GetSdk()->GetPHSdk();
     PHSceneIf *phscene = GetSdk()->GetScene()->GetPHScene();
+    Quaterniond q = Quaterniond::Rot(Rad(45.0), Vec3d(1, 1, 1));
+//    Matrix3d rot;
+//    q.ToMatrix(rot);
     CDBoxDesc bd;
+    PHSolidDesc sd;
+    sd.mass = 1.0;
 
     PHSolidIf *floor = phscene->CreateSolid();
     floor->SetDynamical(false);
+    floor->SetMass(1000.0);
     bd.boxsize = Vec3f(5.0f, 1.0f, 5.0f);
     floor->AddShape(phSdk->CreateShape(bd));
     floor->SetFramePosition(Vec3d(0, -1.0, 0));
 
-    PHSolidIf *box = phscene->CreateSolid();
+    PHSolidIf *box = phscene->CreateSolid(sd);
     bd.boxsize = Vec3f(0.2f, 0.2f, 0.2f);
     box->AddShape(phSdk->CreateShape(bd));
-    box->SetFramePosition(Vec3d(0.0, 1.0, 0.0));
+    box->SetCenterPosition(Vec3d(0.0, 1.0, 0.0));
+    box->SetVelocity(Vec3d(0.0, 0.5, 0.0));
+    box->SetOrientation(q);
+
+    PHSolidIf *sol = phscene->CreateSolid(sd); // sol->SetMass(5.0); // etc
+    bd.boxsize = Vec3f(0.5f, 0.3f, 0.3f);
+    sol->AddShape(phSdk->CreateShape(bd));
+    sol->SetCenterPosition(Vec3d(0.0, 1.5, 0.0));
+    sol->SetVelocity(Vec3d(0.0, 0.5, 0.0));
+    sol->SetAngularVelocity(-Vec3d(0.5, 0.5, 0.5));
+
     GetSdk()->SetDebugMode(true);
   }
 } app;
